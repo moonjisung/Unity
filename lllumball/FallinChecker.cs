b3649a67a3842ba8007c6b5d89a8d51a048971c2
moonjisung <zpfhfh1537@naver.com>
2020-08-25 16:12:22 +0900

    유니티작업

---
+++ lllumball/FallinChecker.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallinChecker : MonoBehaviour {
+	public Hole red;
+	public Hole blue;
+	public Hole green;
+	void OnGUI()
+	{
+		string label = "";
+		if (red.isFallIn() && blue.isFallIn() && green.isFallIn())
+		{
+			label = "Fall in hole";
+		}
+		GUI.Label(new Rect(0,0,100,30),label);
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
